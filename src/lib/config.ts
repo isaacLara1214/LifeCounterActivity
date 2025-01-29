991cd660a068428e000ec9df46eadbc7d4aeab5a
Isaac Lara <isaac.lara014@gmail.com>
2025-01-28 22:58:41 -0500

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
-    COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
-    PRODUCT_NAME: "Just Another Generic Roguelike", // Game Name
+    CLIENT_ID: "1333988697003134996", // Discord Application ID
+    COMPANY_NAME: "Krazzy2eyezz", // Company or Developer Name
+    PRODUCT_NAME: "Commander Life Counter", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
 };
