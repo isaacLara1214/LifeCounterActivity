05cb17a111fe04fe7c720308b990cb81aade6e18
Isaac Lara <isaac.lara014@gmail.com>
2025-01-30 01:09:47 -0500

    added lifecounter app
    fixed issue where built version would not be recognized.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    CLIENT_ID: "1333988697003134996", // Discord Application ID
+    CLIENT_ID: "1334394164749795410", // Discord Application ID
     COMPANY_NAME: "Krazzy2eyezz", // Company or Developer Name
     PRODUCT_NAME: "Commander Life Counter", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
